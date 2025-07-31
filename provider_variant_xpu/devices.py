6e2538fbf116cdeb24e0bf420115d594eefe8886
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2025-07-30 18:47:29 -0700

    Use L0 to query device information
    Using L0 allows to unify Linux and Windows code paths to query
    device information. This also adds implicit check that the driver
    stack is actually installed and is functional for each enumerated
    device.
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- provider_variant_xpu/devices.py
+++ provider_variant_xpu/devices.py
@@ -1,28 +1,32 @@
+# Copyright (c) 2025 Intel Corporation
+
 # Dictionary to match device ids of Intel GPUs with respective device type
 # names as known to ocloc (see `-device` option description in `ocloc compile --help`)
 #
 # Support notes:
+# * Device types MUST use [a-z0-9_.] character set
 # * Add newer hardware on top
 # * Add device types with better performance first
 # * Refer hardware tabel in https://dgpu-docs.intel.com/devices/hardware-table.html
 devices = {
-    "0x0bd5": [ "pvc" ],
-    "0x0bda": [ "pvc" ],
-    "0xe20b": [ "bmg" ],
-    "0xe20c": [ "bmg" ],
-    "0x64a0": [ "lnl" ],
-    "0x6420": [ "lnl" ],
-    "0x64b0": [ "lnl" ],
-    "0x7d51": [ "arl-h" ],
-    "0x7d67": [ "arl-s" ],
-    "0x7d41": [ "arl-u" ],
-    "0x7dd5": [ "mtl" ],
-    "0x7d45": [ "mtl" ],
-    "0x7d40": [ "mtl" ],
-    "0x7d55": [ "mtl" ],
-    "0x56a0": [ "dg2" ],
-    "0x56a1": [ "dg2" ],
-    "0x56a2": [ "dg2" ],
-    "0x56a5": [ "dg2" ],
-    "0x56a6": [ "dg2" ],
+    0x0bd5: [ "pvc" ],
+    0x0bda: [ "pvc" ],
+    0xe20b: [ "bmg" ],
+    0xe20c: [ "bmg" ],
+    0x64a0: [ "lnl" ],
+    0x6420: [ "lnl" ],
+    0x64b0: [ "lnl" ],
+    0x7d51: [ "arl_h" ],
+    0x7d67: [ "arl_s" ],
+    0x7d41: [ "arl_u" ],
+    0x7dd5: [ "mtl" ],
+    0x7d45: [ "mtl" ],
+    0x7d40: [ "mtl" ],
+    0x7d55: [ "mtl" ],
+    0x56a0: [ "dg2" ],
+    0x56a1: [ "dg2" ],
+    0x56a2: [ "dg2" ],
+    0x56a5: [ "dg2" ],
+    0x56a6: [ "dg2" ],
+    0x46a6: [ "adl_p" ],
 }
